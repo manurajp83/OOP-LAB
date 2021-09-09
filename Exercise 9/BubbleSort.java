24cee55ec2a7877410624ad64e5873970921a6b0
manurajp83 <74589077+manurajp83@users.noreply.github.com>
2021-09-10 00:39:38 +0530

    Ex 9

---
+++ Exercise 9/BubbleSort.java
@@ -0,0 +1,64 @@
+/*
+Using generic method perform Bubble sort.
+*/
+
+import java.util.*;
+import java.util.Arrays;
+
+class BubbleSort{
+	public static <T> T[] BSMethod(T[] arr,int n) {
+		String s1,s2;
+		for (int i = 0; i < n-1; i++)   //sorting using bubble sort
+        		for (int j = 0; j < n-i-1; j++){
+				s1 = arr[j].toString();
+				s2 = arr[j+1].toString();
+               			if (s1.compareTo(s2)>0)
+                		{
+                    			// swap arr[j+1] and arr[j]
+                    			T temp = arr[j];
+                    			arr[j] = arr[j+1];
+                    			arr[j+1] = temp;
+                		}
+			}
+		return arr;
+	}
+}
+
+class Main {
+	public static void main(String args[]){
+		 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of elements : ");
+		int n = sc.nextInt();
+		
+		String[] s = new String[n];
+
+		System.out.println("Enter "+n+" elements : ");
+		for(int i = 0 ; i < n ; i++)
+			s[i] = sc.next();
+
+		BubbleSort b = new BubbleSort();
+
+		s = b.<String>BSMethod(s,n);
+		
+		System.out.println("After Sorting :");
+
+		for(int i = 0 ; i < n ; i++)
+			System.out.println(s[i]);
+	}
+}
+
+/*
+Output:
+Enter the number of elements : 4
+Enter 4 elements :
+orange
+apple
+mango
+banana
+After Sorting :
+apple
+banana
+mango
+orange
+*/
---
+++ Exercise 9/Generic1.java
@@ -0,0 +1,110 @@
+/*
+Program to create a generic stack and do the Push and Pop
+operations.
+*/
+
+import java.util.*;
+
+class Stack<T> {									//create a class Stack for push,pop element of a stack
+	ArrayList<T> elements;
+	Stack(int capacity )
+	{
+		elements = new ArrayList<T>(capacity);  
+	}
+	void push( T element)								//to push elements
+	{
+		elements.add(element);
+	}
+	T pop()	
+	{
+		return elements.remove(elements.size()-1);				//to pop last enterd item
+	}
+}
+
+class GenStack{
+	public static void main(String args[]){
+		Integer c,ch,n,top=-1;
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("Enter the capacity of the stack :");
+		c =sc.nextInt();
+		Stack<Integer> st = new Stack<Integer>(c);
+		
+		do{     System.out.println("----------------------");
+			System.out.println("1.PUSH \n2.POP \n3.EXIT");
+			System.out.print("Enter your choice : ");
+			ch = sc.nextInt();
+			switch(ch){
+				case 1: if(top >= (c-1)){
+						System.out.println("Stack is full");
+					}
+					else{
+						System.out.print("Enter a value : ");
+						n = sc.nextInt();
+						st.push(n);
+						top++;
+					}
+     					break;
+				case 2: if(top <= -1){
+						System.out.println("Stack is empty");			
+					}
+					else{
+						 
+						System.out.println("Removed element is "+st.pop());
+						top--;
+					}
+					break;
+			}
+		}while(ch != 3);
+	}
+}
+	
+/*
+Output:
+Enter the capacity of the stack :
+4
+----------------------
+1.PUSH
+2.POP
+3.EXIT
+Enter your choice : 2
+Stack is empty
+----------------------
+1.PUSH
+2.POP
+3.EXIT
+Enter your choice : 1
+Enter a value : 1
+----------------------
+1.PUSH
+2.POP
+3.EXIT
+Enter your choice : 1
+Enter a value : 2
+----------------------
+1.PUSH
+2.POP
+3.EXIT
+Enter your choice : 1
+Enter a value : 3
+----------------------
+1.PUSH
+2.POP
+3.EXIT
+Enter your choice : 1
+Enter a value : 4
+----------------------
+1.PUSH
+2.POP
+3.EXIT
+Enter your choice : 1
+Stack is full
+----------------------
+1.PUSH
+2.POP
+3.EXIT
+Enter your choice : 3
+
+*/
+
+
+		
